0bf9af7fd5f75d3453b22cf78692b0764de64e2e
exmileco <exmile.co@gmail.com>
2019-10-07 22:41:07 +0300

    added modal window, js, jquery open, close functionality, modal window close timer, arrow scroll up jquery functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,17 @@ var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
 var closeModalTimeout = 5000;
+var closeTimeoutId;
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function() {
   modal.classList.add('modal_active');
+  closeTimeoutId = window.setTimeout(function () {
+    modal.classList.remove('modal_active');
+  }, closeModalTimeout);
+  window.addEventListener('input', function () {
+    window.clearTimeout(closeTimeoutId);
+  }, true);
 });
-
 close.addEventListener('click', function(){
   modal.classList.remove('modal_active');
 });
-
-setTimeout((closeModalTimeout) => {
-  modal.classList.remove('modal_active');
-}, closeModalTimeout);
